5edbf4789a1bc00824d4fb2d41b63fd9d5489437
yash goel <yashgoel236437@gmail.com>
2022-01-31 22:24:15 +0530

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-import uvicorn
-from fastapi import FastAPI, File
+from fastapi import FastAPI
 from model import predict_box
+from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+
+class image_base64(BaseModel):
+    img_base64: str
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/")
-def read_image(file: bytes = File(...)):
-    a = predict_box(file)
+def read_image(file: image_base64):
+    base64 = file.img_base64
+    a = predict_box(base64)
     return {"result": a}
 
 
